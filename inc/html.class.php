5dcc716ef92507d73f01783619e361c1aa8a3103
David Durieux <david@durieux.family>
2021-03-17 22:17:26 +0100

    Update copyright glpisc

--- inc/html.class.php
+++ inc/html.class.php
@@ -6439,7 +6439,12 @@ JAVASCRIPT;
     * @return string HTML copyright
     */
    static function getCopyrightMessage($withVersion = true) {
-      $message = "<a href=\"http://glpi-project.org/\" title=\"Powered by Teclib and contributors\" class=\"copyright\">";
+      $message = '<a href="https://github.com/DCS-Easyware/glpi_sc_dcs">GLPISC (secure and clean) ';
+      if ($withVersion) {
+          $message .= GLPI_VERSION;
+      }
+      $message .= '</a> <a href="https://www.dcsit-group.com/">by DCS EASYWARE</a><br>'; 
+      $message .= "<a href=\"http://glpi-project.org/\" title=\"Powered by Teclib and contributors\" class=\"copyright\">";
       $message .= "GLPI ";
       // if required, add GLPI version (eg not for login page)
       if ($withVersion) {
