a54d75d456789fa5b7094f538a9dae5b357ed337
David Durieux <david@durieux.family>
2022-11-16 09:17:39 +0100

    Fix update + prepare version 9.5.10

--- inc/update.class.php
+++ inc/update.class.php
@@ -501,6 +501,9 @@ class Update extends CommonGLPI {
             update957to958();
             break;
 
+         case "9.5.7":
+         case "9.5.8":
+         case "9.5.9":
          case GLPI_VERSION:
          case GLPI_SCHEMA_VERSION:
             break;
