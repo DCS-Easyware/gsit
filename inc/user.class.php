f79ecb037e34be9a8dcac59e80db19ccd18ac115
David Durieux <david@durieux.family>
2024-05-07 10:43:52 +0200

    Fix problem for sync ldap when use PHP 8.1

--- inc/user.class.php
+++ inc/user.class.php
@@ -1587,7 +1587,7 @@ class User extends CommonDBTM {
          return false;
       }
 
-      if (is_resource($ldap_connection)) {
+      if ($ldap_connection) {
          //Set all the search fields
          $this->fields['password'] = "";
 
